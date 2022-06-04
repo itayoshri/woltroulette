fe2bc73fdaf523b3f9dc2846f2f5e4c470ae05f8
Itay Oshri <itay.oshri@gmail.com>
2022-06-04 15:00:37 +0300

    TEST: Api

--- app/pages/api/hello.ts
+++ app/pages/api/hello.ts
@@ -1,13 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { Cordinations, IRestaurantResponseWolt } from '../../interfaces/wolt'
+import { fetchDataSource } from '../../utils/data/datasource'
 
-type Data = {
-  name: string
+import { NextApiRequest, NextApiResponse } from 'next'
+import { WOLT } from '../../utils/data/wolt'
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { location } = _req.query
+    const cordinations = (location as string).split(',') as Cordinations
+    const data = await fetchDataSource<IRestaurantResponseWolt>('restaurants', {
+      location: cordinations,
+    })
+
+    const resturant = WOLT.toIRestaurant(data.sections[1].items[0])
+
+    res.status(200).json(resturant)
+  } catch (err: any) {
+    res.status(500).json({ statusCode: 500, message: err.message })
+  }
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default handler
