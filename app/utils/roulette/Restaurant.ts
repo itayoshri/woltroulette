f9ce7a7985f6f41a94623b66b4b0b87f06eaffe0
Itay Oshri <itay.oshri@gmail.com>
2022-06-04 16:32:59 +0300

    REFACTOR: Restaurant Roulette

--- app/utils/roulette/Restaurant.ts
+++ app/utils/roulette/Restaurant.ts
@@ -1,5 +1,5 @@
 import { IRestaurant } from '../../interfaces/restaurant'
-import { IRestaurantResponseWolt } from '../../interfaces/wolt'
+import { IMenuItemWolt, IRestaurantResponseWolt } from '../../interfaces/wolt'
 import random from '../data/random'
 import { WOLT } from '../data/wolt'
 import { Restaurant } from '../restaurant/Restaurant'
@@ -21,8 +21,29 @@ export class RestaurantRoulette {
     let randomIndex = random(0, this.restaurants.length)
     const restaurant = new Restaurant(this.restaurants[randomIndex].name)
     const items = await restaurant.getItems()
-    randomIndex = random(0, items.length)
-    const randomItem = items[randomIndex]
+    const randomItem = await this.ItemsLottery(items)
     return randomItem
+  }
+
+  private isValidItem(item: IMenuItemWolt): boolean {
+    return true
+  }
+
+  private async ItemsLottery(items: IMenuItemWolt[]) {
+    let isValid = false
+    let tries = 0
+
+    while (!isValid) {
+      // item lottery
+      const randomIndex = random(0, items.length)
+      const randomItem = items[randomIndex]
+
+      isValid = this.isValidItem(randomItem)
+      if (isValid) return randomItem
+
+      // break the loop if too many tries
+      tries++
+      if (tries > items.length) return false
+    }
   }
 }
