a22f21f5ee1c35ccb93cbcd49f74062a16580b6e
Itay Oshri <itay.oshri@gmail.com>
2022-06-11 14:58:15 +0300

    REFACTOR: Results

---
+++ components/forms/Results.tsx
@@ -0,0 +1,25 @@
+export interface IPlaceResult {
+  placeId: string
+  placeName: string
+}
+
+export interface ResultsProps {
+  results: IPlaceResult[]
+  onClick(index): unknown
+}
+
+export default function Results({ results, onClick }: ResultsProps) {
+  return (
+    <div className="flex flex-col w-full bg-white shadow-xl shadow-black/20 border-gray-300 border-2 overflow-hidden rounded-xl absolute top-14">
+      {results.map((result, index) => (
+        <button
+          key={index}
+          onClick={() => onClick(index)}
+          className="w-full py-1 text-right px-3"
+        >
+          {result.placeName}
+        </button>
+      ))}
+    </div>
+  )
+}
