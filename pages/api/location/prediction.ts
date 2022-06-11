c62169a01ceed3a21c3fcb560f5f574f62e577f3
Itay Oshri <itay.oshri@gmail.com>
2022-06-11 15:36:49 +0300

    FIX: Unnecessary req

--- pages/api/location/prediction.ts
+++ pages/api/location/prediction.ts
@@ -10,19 +10,15 @@ const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { search } = _req.query
 
-    if (search === '') {
-      res.status(200).json([])
-    } else {
-      const { predictions } = await fetchDataSource('placeId', {
-        address: search as string,
-      })
+    const { predictions } = await fetchDataSource('placeId', {
+      address: search as string,
+    })
 
-      res.status(200).json(
-        predictions.map((prediction) => {
-          return { place: prediction.description, placeId: prediction.place_id }
-        }) as IPrediction[]
-      )
-    }
+    res.status(200).json(
+      predictions.map((prediction) => {
+        return { place: prediction.description, placeId: prediction.place_id }
+      }) as IPrediction[]
+    )
   } catch (err: any) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
