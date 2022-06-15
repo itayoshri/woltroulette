5ab0d793813545dd91cd4642e241d0ca312b7ec5
Itay Oshri <itay.oshri@gmail.com>
2022-06-15 15:02:42 +0300

    FEATURE: Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import { buildTitleGetStaticProps } from '../components/DocumentHead'
 import LocationInput from '../components/forms/Location'
+import Layout from '../components/Layout'
 import Lottery from '../components/Lottery'
 import Message from '../components/Message'
 import Tabs from '../components/Tabs'
@@ -16,20 +17,18 @@ const Home: NextPage = () => {
   const { tab, platform, message, setTab } = useProvider()
 
   return (
-    <>
-      <div className="flex py-4 flex-col items-center h-full overflow-hidden max-h-screen absolute w-full">
-        {message && <Message />}
-        <Top>
-          <LocationInput />
-        </Top>
-        <Tabs onChange={(number) => setTab(number)} />
-        {tab == 0 ? (
-          <Lottery lotteryType="item" key={0} />
-        ) : (
-          <Lottery lotteryType="restaurant" key={1} />
-        )}
-      </div>
-    </>
+    <Layout>
+      {message && <Message />}
+      <Top>
+        <LocationInput />
+      </Top>
+      <Tabs onChange={(number) => setTab(number)} />
+      {tab == 0 ? (
+        <Lottery lotteryType="item" key={0} />
+      ) : (
+        <Lottery lotteryType="restaurant" key={1} />
+      )}
+    </Layout>
   )
 }
 
