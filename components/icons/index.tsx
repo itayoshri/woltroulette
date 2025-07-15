879398634de17702cf97756c867cc3c9436a70d2
Itay Oshri <itay.oshri@gmail.com>
2025-07-15 23:19:43 +0300

    FEATURE: Icons

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -8,3 +8,13 @@ export const Expand = createSVGResource(
   <path d="M24 24H0V0h24v24z" fill="none" opacity=".87" />,
   <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z" />
 )
+
+export const Share = createSVGResource(
+  <path d="M0 0h24v24H0V0z" fill="none" />,
+  <path d="M16 5l-1.42 1.42-1.59-1.59V16h-1.98V4.83L9.42 6.42 8 5l4-4 4 4zm4 5v11c0 1.1-.9 2-2 2H6c-1.11 0-2-.9-2-2V10c0-1.11.89-2 2-2h3v2H6v11h12V10h-3V8h3c1.1 0 2 .89 2 2z" />
+)
+
+export const Generate = createSVGResource(
+  <path d="M0 0h24v24H0z" fill="none" />,
+  <path d="M22 2l-2.5 1.4L17 2l1.4 2.5L17 7l2.5-1.4L22 7l-1.4-2.5zm-7.63 5.29c-.39-.39-1.02-.39-1.41 0L1.29 18.96c-.39.39-.39 1.02 0 1.41l2.34 2.34c.39.39 1.02.39 1.41 0L16.7 11.05c.39-.39.39-1.02 0-1.41l-2.33-2.35zm-1.03 5.49l-2.12-2.12 2.44-2.44 2.12 2.12-2.44 2.44z" />
+)
